87127eceb6a85714aa73b9772307cc1d2e79ede0
islam <islamhm1998@gmail.com>
2021-01-31 13:13:49 +0200

    devicenetwork

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from helper import *
+import pytest
 
 file_name = 'NetworkDevice.csv'
-
+count = 5
 devices_list =read_file(file_name)
-
-for device in devices_list:
-    print (device.device_type,device.pid,device.name,device.ip_address,device.user_name,device.password,device.snmp_read_community)
-
+#for device in devices_list:
+   # print (device.device_type,device.pid,device.name,device.ip_address,device.user_name,device.password,device.snmp_read_community)
+#print (len(devices_list))
+def test_device_count():
+   assert len(devices_list)==count
